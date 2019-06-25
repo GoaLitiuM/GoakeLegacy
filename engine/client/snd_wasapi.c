b19e18201674cc325861a37bc6a6b4a79acc0c4f
GoaLitiuM <goalitium@kapsi.fi>
2019-06-25 03:52:08 +0300

    Support low latency shared WASAPI streams on Windows 10
    Minimum buffer times are also enforced, so snd_mixahead value of 0 should initialize the audio streams with lowest supported latency.
    
    wasapi_buffersize is now only used with wasapi_exclusive, and snd_mixahead with non-exclusive streams.
    
    wasapi_forcechannels defaults to 1 as the latest Windows releases now use "channel phantoming" to present more audio channels for stereo outputs, which would be incorrectly detected as multi-channel audio output. User should manually define the number of channels with snd_numchannels instead of relying unreliable autodetection.

--- engine/client/snd_wasapi.c
+++ engine/client/snd_wasapi.c
@@ -26,15 +26,17 @@
 FORCE_DEFINE_GUID(CLSID_MMDeviceEnumerator,			0xBCDE0395, 0xE52F, 0x467C, 0x8E, 0x3D, 0xC4, 0x57, 0x92, 0x91, 0x69, 0x2E);
 FORCE_DEFINE_GUID(IID_IMMDeviceEnumerator,			0xA95664D2, 0x9614, 0x4F35, 0xA7, 0x46, 0xDE, 0x8D, 0xB6, 0x36, 0x17, 0xE6);
 FORCE_DEFINE_GUID(IID_IAudioClient,					0x1CB9AD4C, 0xDBFA, 0x4c32, 0xB1, 0x78, 0xC2, 0xF5, 0x68, 0xA7, 0x03, 0xB2);
+FORCE_DEFINE_GUID(IID_IAudioClient3,				0x7ED4EE07, 0x8E67, 0x4CD4, 0x8C, 0x1A, 0x2B, 0x7A, 0x59, 0x87, 0xAD, 0x42);
 FORCE_DEFINE_GUID(IID_IAudioRenderClient,			0xF294ACFC, 0x3146, 0x4483, 0xA7, 0xBF, 0xAD, 0xDC, 0xA7, 0xC2, 0x60, 0xE2);
 
 FORCE_DEFINE_GUID(KSDATAFORMAT_SUBTYPE_PCM,			0x00000001, 0x0000, 0x0010, 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71);
 FORCE_DEFINE_GUID(KSDATAFORMAT_SUBTYPE_IEEE_FLOAT,	0x00000003, 0x0000, 0x0010, 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71);
 
 static cvar_t wasapi_forcerate		= CVARD("wasapi_forcerate", "0", "Attempts to force snd_khz instead of using the system's default channel count.\nFor this to work, you will need to set wasapi_exclusive 1");
-static cvar_t wasapi_forcechannels	= CVARD("wasapi_forcechannels", "0", "Attempts to force snd_numchannels instead of using the system's default channel count.\nFor this to work, you will need to set wasapi_exclusive 1");
+static cvar_t wasapi_forcechannels	= CVARD("wasapi_forcechannels", "1", "Attempts to force snd_numchannels instead of using the system's default channel count.");
 static cvar_t wasapi_exclusive		= CVARD("wasapi_exclusive", "0", "When set, attempts to take exclusive control of the output device, to the detriment of other programs (causing errors or even crashes in them).\nExclusive mode leaves the game free to change the hardware's playback details, instead of being required to use a single system-wide 'mixer' rate.\nIt should also reduce latency a little.");
-static cvar_t wasapi_buffersize		= CVAR("wasapi_buffersize", "0.01");
+static cvar_t wasapi_buffersize		= CVARD("wasapi_buffersize", "0.01", "Internal buffer size in seconds used for WASAPI output.\nFor this to work, you will need to set wasapi_exclusive 1");
+extern cvar_t _snd_mixahead;
 static void QDECL WASAPI_RegisterCvars(void)
 {
 	Cvar_Register(&wasapi_forcerate, "WASAPI audio output");
@@ -45,6 +47,7 @@ static void QDECL WASAPI_RegisterCvars(void)
 
 static void *WASAPI_Lock(soundcardinfo_t *sc, unsigned int *startoffset)
 {
+	*startoffset = 0;
 	return sc->sn.buffer;
 }
 static void WASAPI_Unlock(soundcardinfo_t *sc, void *buffer)
@@ -74,7 +77,7 @@ static qboolean WASAPI_AcceptableFormat(soundcardinfo_t *sc, IAudioClient *dev, 
 		sc->sn.samplebytes = 4;
 		sc->sn.sampleformat = QSF_F32;
 	}
-	else if (pwfx->wFormatTag == WAVE_FORMAT_EXTENSIBLE && memcmp(&((WAVEFORMATEXTENSIBLE*)pwfx)->SubFormat, &KSDATAFORMAT_SUBTYPE_PCM, sizeof(GUID))) 
+	else if (pwfx->wFormatTag == WAVE_FORMAT_EXTENSIBLE && memcmp(&((WAVEFORMATEXTENSIBLE*)pwfx)->SubFormat, &KSDATAFORMAT_SUBTYPE_PCM, sizeof(GUID)))
 	{
 		Con_Printf("WASAPI: unsupported sample type\n");
 		return false;	//we only support pcm / floats
@@ -124,7 +127,7 @@ static qboolean WASAPI_DetermineFormat(soundcardinfo_t *sc, IAudioClient *dev, q
 
 	if (wasapi_forcechannels.ival)
 	{
-		Con_Printf("WASAPI: overriding channels\n");
+		Con_DPrintf("WASAPI: overriding channels\n");
 
 		if (sc->sn.numchannels >= 8)
 		{
@@ -237,8 +240,10 @@ static int WASAPI_Thread(void *arg)
 	soundcardinfo_t *sc = arg;
 	qboolean inited = false;
 
-//	REFERENCE_TIME hnsRequestedDuration = REFTIMES_PER_SEC;
 	IAudioClient *pAudioClient = NULL;
+#if _WIN32_WINNT >= 0x0A00 // Windows 10 (_WIN32_WINNT_WIN10)
+	IAudioClient3 *pAudioClient3 = NULL;
+#endif
 	IAudioRenderClient *pRenderClient = NULL;
 	UINT32 bufferFrameCount = 0;
 	HANDLE hEvent = NULL;
@@ -252,80 +257,177 @@ static int WASAPI_Thread(void *arg)
 	IMMDevice *pDevice = WASAPI_GetDevice(sc);
 	if (pDevice)
 	{
-		if (SUCCEEDED(pDevice->lpVtbl->Activate(pDevice, &IID_IAudioClient, CLSCTX_ALL, NULL, (void**)&pAudioClient)))
+		UINT32 periodicity_in_frames = 0;
+		HRESULT hr;
+#if _WIN32_WINNT >= 0x0A00
+		if (!exclusive && SUCCEEDED(pDevice->lpVtbl->Activate(pDevice, &IID_IAudioClient3, CLSCTX_ALL, NULL, (void**)&pAudioClient3)))
+			hr = pAudioClient3->lpVtbl->QueryInterface(pAudioClient3, &IID_IAudioClient, (void**)&pAudioClient);
+		else
+#endif
 		{
-			if (!WASAPI_DetermineFormat(sc, pAudioClient, exclusive, &pwfx))
+			hr = pDevice->lpVtbl->Activate(pDevice, &IID_IAudioClient, CLSCTX_ALL, NULL, (void**)&pAudioClient);
+		}
+
+		if (FAILED(hr))
+		{
+			Con_Printf("WASAPI: failed to activate audio client\n");
+		}
+		else if (!WASAPI_DetermineFormat(sc, pAudioClient, exclusive, &pwfx))
+		{
+			Con_Printf("WASAPI: unable to determine mutually supported audio format\n");
+		}
+		else if (sc->sn.samplebytes && (!snd_speed || sc->sn.speed == snd_speed))
+		{
+#if _WIN32_WINNT >= 0x0A00
+			if (pAudioClient3)
 			{
-				Con_Printf("WASAPI: unable to determine mutually supported audio format\n");
-			}
-			else
-			{
-				if (sc->sn.samplebytes && (!snd_speed || sc->sn.speed == snd_speed))
+				UINT32 default_period;
+		        UINT32 fundamental_period;
+		        UINT32 min_period;
+		        UINT32 max_period;
+
+		        AudioClientProperties properties = { 0 };
+		        properties.cbSize = sizeof(AudioClientProperties);
+		        properties.Options = AUDCLNT_STREAMOPTIONS_NONE;
+
+		        if (SUCCEEDED(IAudioClient2_SetClientProperties(pAudioClient3, &properties)) &&
+		            SUCCEEDED(IAudioClient3_GetSharedModeEnginePeriod(pAudioClient3, pwfx,
+		                &default_period, &fundamental_period, &min_period, &max_period)))
+		        {
+		            periodicity_in_frames = fundamental_period * (UINT32)(pwfx->nSamplesPerSec *
+		                _snd_mixahead.value / 2 / fundamental_period);
+
+		            if (periodicity_in_frames > max_period)
+					{
+		               	pAudioClient3->lpVtbl->Release(pAudioClient3);
+		                pAudioClient3 = NULL;
+		            }
+					else if (periodicity_in_frames < min_period)
+						periodicity_in_frames = min_period;
+
+					Con_DPrintf("WASAPI: requested periodicity: %d, default: %d, min: %d, max: %d, step: %d\n", periodicity_in_frames, default_period, min_period, max_period, fundamental_period);
+		        }
+				else
 				{
-					HRESULT hr;
-					REFERENCE_TIME buffersize = REFTIMES_PER_SEC * wasapi_buffersize.value;
-					if (exclusive)
-						pAudioClient->lpVtbl->GetDevicePeriod(pAudioClient, NULL, &buffersize);
+		            pAudioClient3->lpVtbl->Release(pAudioClient3);
+		            pAudioClient3 = NULL;
+				}
+			}
 
-					hr = pAudioClient->lpVtbl->Initialize(pAudioClient, exclusive?AUDCLNT_SHAREMODE_EXCLUSIVE:AUDCLNT_SHAREMODE_SHARED, AUDCLNT_STREAMFLAGS_EVENTCALLBACK, buffersize, (exclusive?buffersize:0), pwfx, NULL);
+			if (pAudioClient3)
+			{
+				hr = IAudioClient3_InitializeSharedAudioStream(pAudioClient3,
+	            	AUDCLNT_STREAMFLAGS_EVENTCALLBACK, periodicity_in_frames, pwfx, NULL);
+				if (hr == AUDCLNT_E_ENGINE_PERIODICITY_LOCKED)
+				{
+					IAudioClient3_GetCurrentSharedModeEnginePeriod(pAudioClient3, &pwfx, &periodicity_in_frames);
+					Con_Printf("WASAPI: Periodicity locked to %d\n", periodicity_in_frames);
+					hr = IAudioClient3_InitializeSharedAudioStream(pAudioClient3,
+	            		AUDCLNT_STREAMFLAGS_EVENTCALLBACK, periodicity_in_frames, pwfx, NULL);
+				}
 
-					if (hr == AUDCLNT_E_BUFFER_SIZE_NOT_ALIGNED)
-					{	//this is stupid, but does what the documentation says should be done.
-						if (SUCCEEDED(pAudioClient->lpVtbl->GetBufferSize(pAudioClient, &bufferFrameCount)))
-						{
-							if (pAudioClient)
-								pAudioClient->lpVtbl->Release(pAudioClient);
-							pAudioClient = NULL;
-							if (SUCCEEDED(pDevice->lpVtbl->Activate(pDevice, &IID_IAudioClient, CLSCTX_ALL, NULL, (void**)&pAudioClient)))
-							{
-								buffersize = (REFERENCE_TIME)((10000.0 * 1000 / pwfx->nSamplesPerSec * bufferFrameCount) + 0.5);
-								hr = pAudioClient->lpVtbl->Initialize(pAudioClient, exclusive?AUDCLNT_SHAREMODE_EXCLUSIVE:AUDCLNT_SHAREMODE_SHARED, AUDCLNT_STREAMFLAGS_EVENTCALLBACK, buffersize, (exclusive?buffersize:0), pwfx, NULL);
-							}
-						}
-					}
+				if (SUCCEEDED(hr))
+		        {
+					Con_Printf("WASAPI: Low latency mode enabled\n");
+		        }
+				else
+				{
+					pAudioClient3->lpVtbl->Release(pAudioClient3);
+	            	pAudioClient3 = NULL;
+				}
+			}
 
-					if (SUCCEEDED(hr))
+			if (!pAudioClient3 && pAudioClient)
+#endif
+			{
+				REFERENCE_TIME buffersize, minBuffersize, defaultBuffersize;
+
+				// minimum buffer size is used with exclusive mode, default is the recommended (minimum) buffer size for shared mode
+				pAudioClient->lpVtbl->GetDevicePeriod(pAudioClient, &defaultBuffersize, &minBuffersize);
+
+				if (exclusive)
+				{
+					buffersize = REFTIMES_PER_SEC * wasapi_buffersize.value;
+					if (buffersize < minBuffersize)
+						buffersize = minBuffersize;
+				}
+				else
+				{
+					buffersize = REFTIMES_PER_SEC * _snd_mixahead.value;
+					if (buffersize < defaultBuffersize)
+						buffersize = defaultBuffersize;
+				}
+
+				hr = pAudioClient->lpVtbl->Initialize(pAudioClient, exclusive ? AUDCLNT_SHAREMODE_EXCLUSIVE : AUDCLNT_SHAREMODE_SHARED, AUDCLNT_STREAMFLAGS_EVENTCALLBACK, buffersize, (exclusive?buffersize:0), pwfx, NULL);
+
+				if (hr == AUDCLNT_E_BUFFER_SIZE_NOT_ALIGNED)
+				{	//this is stupid, but does what the documentation says should be done.
+					if (SUCCEEDED(pAudioClient->lpVtbl->GetBufferSize(pAudioClient, &bufferFrameCount)))
 					{
-						hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
-						if (hEvent)
+						if (pAudioClient)
+							pAudioClient->lpVtbl->Release(pAudioClient);
+						pAudioClient = NULL;
+						if (SUCCEEDED(pDevice->lpVtbl->Activate(pDevice, &IID_IAudioClient, CLSCTX_ALL, NULL, (void**)&pAudioClient)))
 						{
-							pAudioClient->lpVtbl->SetEventHandle(pAudioClient, hEvent);
-							if (SUCCEEDED(pAudioClient->lpVtbl->GetBufferSize(pAudioClient, &bufferFrameCount)))
-							if (SUCCEEDED(pAudioClient->lpVtbl->GetService(pAudioClient, &IID_IAudioRenderClient, (void**)&pRenderClient)))
-								inited = true;
-						}
-					}
-					else
-					{
-						switch(hr)
-						{
-						case AUDCLNT_E_UNSUPPORTED_FORMAT:
-							Con_Printf("WASAPI Initialize: AUDCLNT_E_UNSUPPORTED_FORMAT\n");
-							break;
-						case AUDCLNT_E_EXCLUSIVE_MODE_NOT_ALLOWED:
-							Con_Printf("WASAPI Initialize: AUDCLNT_E_EXCLUSIVE_MODE_NOT_ALLOWED\n");
-							break;
-						case AUDCLNT_E_EXCLUSIVE_MODE_ONLY:
-							Con_Printf("WASAPI Initialize: AUDCLNT_E_EXCLUSIVE_MODE_ONLY\n");
-							break;
-						case AUDCLNT_E_DEVICE_IN_USE:
-							Con_Printf("WASAPI Initialize: AUDCLNT_E_DEVICE_IN_USE\n");
-							break;
-						case AUDCLNT_E_BUFFER_SIZE_NOT_ALIGNED:
-							Con_Printf("WASAPI Initialize: AUDCLNT_E_BUFFER_SIZE_NOT_ALIGNED\n");
-							break;
-						case E_INVALIDARG:
-							Con_Printf("WASAPI Initialize: E_INVALIDARG\n");
-							break;
-						default:
-							Con_Printf("pAudioClient->lpVtbl->Initialize failed (%x)\n", (unsigned int)hr);
+							buffersize = (REFERENCE_TIME)((10000.0 * 1000 / pwfx->nSamplesPerSec * bufferFrameCount) + 0.5);
+							hr = pAudioClient->lpVtbl->Initialize(pAudioClient, exclusive ? AUDCLNT_SHAREMODE_EXCLUSIVE : AUDCLNT_SHAREMODE_SHARED, AUDCLNT_STREAMFLAGS_EVENTCALLBACK, buffersize, (exclusive?buffersize:0), pwfx, NULL);
 						}
 					}
 				}
-
-				CoTaskMemFree(pwfx);
 			}
+
+			if (SUCCEEDED(hr))
+			{
+				hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+				if (hEvent)
+				{
+					pAudioClient->lpVtbl->SetEventHandle(pAudioClient, hEvent);
+					if (SUCCEEDED(pAudioClient->lpVtbl->GetBufferSize(pAudioClient, &bufferFrameCount)) &&
+						SUCCEEDED(pAudioClient->lpVtbl->GetService(pAudioClient, &IID_IAudioRenderClient, (void**)&pRenderClient)))
+					{
+						inited = true;
+						Con_DPrintf("WASAPI: buffer size: %d\n", bufferFrameCount);
+					}
+				}
+			}
+			else
+			{
+				switch(hr)
+				{
+				case AUDCLNT_E_UNSUPPORTED_FORMAT:
+					Con_Printf("WASAPI Initialize: AUDCLNT_E_UNSUPPORTED_FORMAT\n");
+					break;
+				case AUDCLNT_E_EXCLUSIVE_MODE_NOT_ALLOWED:
+					Con_Printf("WASAPI Initialize: AUDCLNT_E_EXCLUSIVE_MODE_NOT_ALLOWED\n");
+					break;
+				case AUDCLNT_E_EXCLUSIVE_MODE_ONLY:
+					Con_Printf("WASAPI Initialize: AUDCLNT_E_EXCLUSIVE_MODE_ONLY\n");
+					break;
+				case AUDCLNT_E_DEVICE_IN_USE:
+					Con_Printf("WASAPI Initialize: AUDCLNT_E_DEVICE_IN_USE\n");
+					break;
+				case AUDCLNT_E_BUFFER_SIZE_NOT_ALIGNED:
+					Con_Printf("WASAPI Initialize: AUDCLNT_E_BUFFER_SIZE_NOT_ALIGNED\n");
+					break;
+				case E_INVALIDARG:
+					Con_Printf("WASAPI Initialize: E_INVALIDARG\n");
+					break;
+				default:
+					Con_Printf("pAudioClient->lpVtbl->Initialize failed (%x)\n", (unsigned int)hr);
+				}
+			}
+#if _WIN32_WINNT >= 0x0A00
+			if (pAudioClient3)
+			{
+				pAudioClient3->lpVtbl->Release(pAudioClient3);
+	            pAudioClient3 = NULL;
+			}
+#endif
 		}
+
+		if (pwfx)
+			CoTaskMemFree(pwfx);
+
 		pDevice->lpVtbl->Release(pDevice);
 		pDevice = NULL;
 	}
@@ -338,14 +440,14 @@ static int WASAPI_Thread(void *arg)
 	sc->Unlock		= WASAPI_Unlock;
 	sc->Submit		= WASAPI_Submit;
 	sc->GetDMAPos	= WASAPI_GetDMAPos;
-	
+
 	//wake up the main thread now that we know if it worked.
 	Sys_ConditionSignal(cond);
 
 	//extra crap to get the OS to favour waking us up on demand.
 	{
 		HANDLE (WINAPI *pAvSetMmThreadCharacteristics)(LPCTSTR TaskName, LPDWORD TaskIndex);
-		dllfunction_t funcs[] = {{(void*)&pAvSetMmThreadCharacteristics, "AvSetMmThreadCharacteristics"}, {NULL}};
+		dllfunction_t funcs[] = {{(void*)&pAvSetMmThreadCharacteristics, "AvSetMmThreadCharacteristicsA"}, {NULL}};
 		DWORD taskIndex = 0;
 
 		if (Sys_LoadLibrary("avrt.dll", funcs))
@@ -357,9 +459,9 @@ static int WASAPI_Thread(void *arg)
 		UINT32 numFramesPadding = 0;
 		if (exclusive || SUCCEEDED(pAudioClient->lpVtbl->GetCurrentPadding(pAudioClient, &numFramesPadding)))
 		{
-			UINT32 numFramesAvailable = bufferFrameCount - numFramesPadding;
+			INT32 numFramesAvailable = bufferFrameCount - numFramesPadding;
 			BYTE *pData;
-			if (SUCCEEDED(pRenderClient->lpVtbl->GetBuffer(pRenderClient, numFramesAvailable, &pData)))
+			if (numFramesAvailable > 0 && SUCCEEDED(pRenderClient->lpVtbl->GetBuffer(pRenderClient, numFramesAvailable, &pData)))
 			{
 				sc->sn.buffer = pData;
 				sc->sn.samples = numFramesAvailable * sc->sn.numchannels;
@@ -382,17 +484,10 @@ static int WASAPI_Thread(void *arg)
 			Con_Printf("WASAPI timeout\n");
 			break;
 		}
-
-		/* Quote:
-		On NT (Win2K and XP) the cursors in SW buffers (and HW buffers on some devices) move in 10ms increments, so calling GetCurrentPosition() every 10ms is ideal.
-		Calling it more often than every 5ms will cause some perf degradation.
-		*/
-//		Sleep(10);
 	}
 
 	if (inited)
 		pAudioClient->lpVtbl->Stop(pAudioClient);
-
 	if (pRenderClient)
 		pRenderClient->lpVtbl->Release(pRenderClient);
 	if (pAudioClient)
